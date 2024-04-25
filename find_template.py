23a8d0d74c5d8c509651a817db0a22125059b4ca
Yuanqing Wu <yuanqing.wu@bytedance.com>
2024-04-25 15:57:09 +0800

    add find template

--- find_template.py
+++ find_template.py
@@ -8,7 +8,7 @@ pro = ts.pro_api()
 
 stock_list = pro.stock_basic(exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,market,list_date')
 # print(stock_list['industry'].value_counts())
-stock_list = stock_list[stock_list['industry'] == '电气设备']
+#stock_list = stock_list[stock_list['industry'] == '电气设备']
 # print(stock_list)
 stock_list = stock_list[~stock_list['ts_code'].str.contains('BJ')]
 stock_list = stock_list[~stock_list['ts_code'].str.startswith('3')]
@@ -29,18 +29,34 @@ for index, stock in stock_list.iterrows():
             # print(daily_basic)
             pe_avg = pe_avg + daily_basic['pe'][0]
             num = num + 1
-            k_line = pro.daily(ts_code=ts_code, start_date='20200409', end_date='20240409')
+            k_line = pro.daily(ts_code=ts_code, start_date='20200409', end_date='20240411')
             max_p = k_line['close'].max()
             min_p = k_line['close'].min()
             cur_week_p = k_line.head(7)
             cur_p = cur_week_p.loc[0, 'close']
+            # print(cur_week_p)
+            # break
             
-            if (cur_p - min_p)/(max_p - cur_p) < 0.2:
+            # if max_p == cur_p or (cur_p - min_p)/(max_p - cur_p) < 0.2:
+            #print(name)
+            # t = 0
+            for i in range(1):
+                open_p = cur_week_p.loc[i, 'open']
+                high_p = cur_week_p.loc[i, 'high']
+                low_p = cur_week_p.loc[i, 'low']
+                close_p = cur_week_p.loc[i, 'close']
+                if (high_p - open_p) / open_p < 0.03 and (close_p - low_p) / open_p > 0.04:
+                # if (close_p - low_p) / open_p > 0.03:
+                    # t = t + 1
+                    # if t > 1:
+                    print(name)
+                            # break
+            
                 #print(cur_week_p.loc[6, 'close'])
-                last_week_p = cur_week_p.loc[6, 'close']
-                if (cur_p - last_week_p) / last_week_p > 0.05 and (cur_p - last_week_p) / last_week_p < 0.1:
-                    var = cur_week_p['close'].var() / cur_p / cur_p
-                    print(name, cur_p, last_week_p, var)
+                # last_week_p = cur_week_p.loc[6, 'close']
+                # if (cur_p - last_week_p) / last_week_p > 0.05 and (cur_p - last_week_p) / last_week_p < 0.1:
+                #     var = cur_week_p['close'].var() / cur_p / cur_p
+                #     print(name, cur_p, last_week_p, var)
             
     
 
